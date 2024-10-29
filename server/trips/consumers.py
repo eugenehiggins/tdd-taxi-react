3570b6d32cafb5708994602a14d0cf63dcd693f6
Gene Higgins <hi@genehiggins.com>
2024-10-29 15:38:14 -0600

    added ability for consumers to know if the group is drivers or users

--- server/trips/consumers.py
+++ server/trips/consumers.py
@@ -1,25 +1,39 @@
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncJsonWebsocketConsumer
 
 
 class TaxiConsumer(AsyncJsonWebsocketConsumer):
     groups = ['test']
 
-    async def connect(self):  # changed
+    @database_sync_to_async
+    def _get_user_group(self, user):
+        return user.groups.first().name
+
+    async def connect(self):
         user = self.scope['user']
         if user.is_anonymous:
             await self.close()
         else:
-            await self.channel_layer.group_add(
-                group='test',
-                channel=self.channel_name
-            )
+            user_group = await self._get_user_group(user)  # new
+            if user_group == 'driver':
+                await self.channel_layer.group_add(
+                    group='drivers',
+                    channel=self.channel_name
+                )
             await self.accept()
 
-    async def disconnect(self, code): # changed
-        await self.channel_layer.group_discard(
-            group='test',
-            channel=self.channel_name
-        )
+    async def disconnect(self, code):
+        user = self.scope['user']  # new
+        # new
+        if user.is_anonymous:
+            await self.close()
+        else:
+            user_group = await self._get_user_group(user)
+            if user_group == 'driver':
+                await self.channel_layer.group_discard(
+                    group='drivers',
+                    channel=self.channel_name
+                )
         await super().disconnect(code)
 
     async def echo_message(self, message):
